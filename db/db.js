937c0c44db0d9dff4b2ff1f5ed8476998827a471
Abdul Rehman Khan <khan456.tdm@gmail.com>
2024-06-28 14:24:52 +0500

    added admin views

--- db/db.js
+++ db/db.js
@@ -93,7 +93,44 @@ DB.fetchUser = async (userEmail) => {
   }
 };
 
+// Function to add a new venue
+DB.addVenue = (venueData) => {
+  return new Promise((resolve, reject) => {
+    // Prepare the SQL query
+    const query = `
+      INSERT INTO venues (name, address, image_url, seating_capacity, charges)
+      VALUES (?, ?, ?, ?, ?)
+    `;
+    
+    // Execute the query with parameters
+    db.run(query, [venueData.name, venueData.address, venueData.pictureUrl, venueData.area, venueData.charges], function(err) {
+      if (err) {
+        console.error('Error adding venue:', err.message);
+        reject(err); // Reject the promise with the error
+      } else {
+        console.log(`Venue added with ID: ${this.lastID}`);
+        resolve({ id: this.lastID }); // Resolve the promise with the last inserted ID
+      }
+    });
+  });
+};
 
+DB.fetchAllVenues = () => {
+  return new Promise((resolve, reject) => {
+    // Prepare the SQL query
+    const query = `SELECT * FROM venues`;
+    
+    // Execute the query
+    db.all(query, [], (err, rows) => {
+      if (err) {
+        console.error('Error fetching venues:', err.message);
+        reject(err); // Reject the promise with the error
+      } else {
+        resolve(rows); // Resolve the promise with the fetched rows
+      }
+    });
+  });
+};
 
 DB.fetchAllUsers = () => {
   const query = `SELECT * FROM users`;
@@ -114,5 +151,17 @@ DB.closeDB = () => {
     console.log('Database connection closed.');
   });
 };
+
+// db.all(`PRAGMA table_info(venues);`, [], (err, rows) => {
+//   if (err) {
+//     console.error('Error fetching table schema:', err.message);
+//     return;
+//   }
+//   console.log('Table schema:', rows);
+// });
+
+db.get(`select * from venues`, [], (err, rows) => {
+  // console.log("Row:", rows);
+});
 
 module.exports = DB;
--- router/adminRoutes.js
+++ router/adminRoutes.js
@@ -2,21 +2,36 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const adminHTMl = require("../utils/renderAdminHTML");
+const DB = require("../db/db");
 
 router.get("/", (req, res) => {
   res.send(adminHTMl.adminDashboard());
 });
 
-router.get("/venue/view", (req, res) => {
-  res.send(adminHTMl.renderAdminView());
+router.get("/venue/view", async (req, res) => {
+  const venues = await DB.fetchAllVenues();
+  res.send(adminHTMl.renderAdminView(venues));
 });
 
 router.get("/venue/new", (req, res) => {
   res.send(adminHTMl.addVenue());
 });
 
-router.get("/venue/edit", (req, res) => {
-  res.send(adminHTMl.editVenue());  
+router.post("/venue/new", (req,res)=>{
+  const data = {
+    name: req.body.name,
+    address: req.body.address,
+    pictureUrl: req.body.pictureUrl,
+    area: req.body.area,
+    charges: req.body.charges
+  }
+  DB.addVenue(data)
+  res.status(200);
+})
+
+router.get("/venue/edit", async (req, res) => {
+  const response = await DB.fetchAllVenues()
+  res.send(adminHTMl.editVenue(response));  
 });
 
 router.get("/venue/delete", (req, res) => {
